a92f6b845851c9fd3fab3acfd2507712ccd0a2d8
MilesHuang <MilesHuang@MILESH>
2018-04-28 16:58:11 +0800

    huang 4.28

--- map/ViewModel/index.js
+++ map/ViewModel/index.js
@@ -1,3 +1,8 @@
+var nameInit = false;
+
+$(function(){
+	$(".selectPage").hide();
+})
 $("#SelectName").click(function(){
 	showFliter();
 	$(".origin").addClass("DarkBody");
@@ -9,6 +14,17 @@ $("#close").click(function(){
 })
 
 $("#check").click(function(){
+	var nameArray = [];
+	$("#NameForm").find("input").each(function(){
+		console.log($(this).checked);
+		if ($(this).is(":checked")) {
+			console.log("yes");
+			nameArray.push($(this).next().text());
+		}
+	});
+	console.log(nameArray);
+	//处理已选中的node（name标识，存在nameArray里）
+
 
 }) 
 
@@ -16,6 +32,32 @@ $("#cancel").click(function(){
 	$(".mycheckbox").attr("checked", false);
 })
 
+/*-------根据已有数据显示筛选框--------*/
 function showFliter() {
-	
+	var count = 0;
+	if (nameInit === false) {	
+		$.getJSON("../Model/relation.json", function(data){
+			$.each(data.nodes, function(infoIndex, info){
+				addName(info["name"],count);
+				count += 1;
+			})
+
+		})
+		$(".selectPage").show();
+		nameInit = true;
+	}
+	else {
+		$(".selectPage").show();
+	}
+
+}
+
+function addName(name,count) {
+	var label = $("<label></label>");
+	label.css('display','block');
+	var input = $("<input></input>").attr('type','checkbox').attr('id',count).attr('class','mycheckbox');
+	label.append(input);
+	label.append("<span>" + name + "</span>");
+	$("#NameForm").append(label);
+
 }
